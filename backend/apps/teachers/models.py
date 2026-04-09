5ffc45a5d513dee4abf5fadd33caca66c288fab8
Francis Mensah <117544815+Cazio88@users.noreply.github.com>
2026-04-09 10:28:48 +0000

    Enhance Teacher model with save method and full_name
    Add save method to set default password for new teachers and a full_name property.

--- backend/apps/teachers/models.py
+++ backend/apps/teachers/models.py
@@ -3,32 +3,37 @@ from django.conf import settings
 from apps.subjects.models import Subject
 from apps.classes.models import SchoolClass
 
-
 class Teacher(models.Model):
-
     teacher_id = models.CharField(
         max_length=20,
         unique=True
     )
-
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
-
     subject = models.ForeignKey(
         Subject,
         on_delete=models.SET_NULL,
         null=True
     )
-
     school_class = models.ForeignKey(
         SchoolClass,
         on_delete=models.SET_NULL,
         null=True
     )
-
     hire_date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        is_new = self._state.adding
+        super().save(*args, **kwargs)
+        if is_new and self.user and not self.user.has_usable_password():
+            self.user.set_password("teacher123")
+            self.user.save(update_fields=["password"])
+
+    @property
+    def full_name(self):
+        return self.user.get_full_name() or self.user.username
 
     def __str__(self):
         return self.teacher_id
